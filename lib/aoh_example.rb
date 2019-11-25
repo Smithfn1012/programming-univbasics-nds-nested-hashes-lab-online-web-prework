8c587fb4a027801491bdada19e8e273fe12ff172
Felicia Smith <smithfn1012@gmail.com>
2019-11-25 18:46:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  
+  literal_aoh = [
+    {:name => "Don Gately", :occupation => "Live-in "}]
 end
 
 def aoh_lookup(aoh, row, key)
